3f39365762d42c985a97960d627207ad05d4e3bb
IrvingDelgadillo <1725110181@utectulancingo.edu.mx>
2026-07-01 14:03:55 +0000

    CREATED guardar

---
+++ agenda/app.py
@@ -0,0 +1,11 @@
+import web
+
+urls = (
+    '/', 'controllers.index.Index',
+    '/lista_contactos','controllers.lista_contactos.ListaContactos',
+    '/ver_contacto/(.*)','controllers.ver_contacto.VerContacto'
+)
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
